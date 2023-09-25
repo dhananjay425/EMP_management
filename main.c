737a4fda73579bafc43e4a7925dd5fb58d44dabf
Dhananjay chaudhary <35595412+dhananjay425@users.noreply.github.com>
2023-09-25 19:39:49 +0530

    Update main.c

--- main.c
+++ main.c
@@ -7,14 +7,15 @@
 int main()
 {
   int choice;
-  
+
   while (1)
   {
     printf("\n********Menu for Employee Management System*******");
     printf("\n1. Add Employee Details.");
-    printf("\n2. Show Employee Details.");
+    printf("\n2. Search Employee Details(By Employee Id).");
     printf("\n3. Delete Employee Details.");
-    printf("\n4. Exit.");
+    printf("\n4. Update Employee Details.");
+    printf("\n5. Exit.");
     printf("\n\n Enter your Choice = ");
     scanf("%d", &choice);
 
@@ -42,6 +43,13 @@ int main()
       system("cls");
       break;
     case 4:
+      system("cls");
+      updateEmployee();
+      printf("\n\nEnter anything to go to Main menu.......");
+      _getch();
+      system("cls");
+      break;
+    case 5:
       exit(1);
     default:
       printf("Invalid Choice.....\n Please enter valid choice.....\n");
