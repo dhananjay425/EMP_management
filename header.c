ad7e8f304c4a84edf79bcdfd46834497eddff664
Dhananjay chaudhary <35595412+dhananjay425@users.noreply.github.com>
2023-09-25 19:38:55 +0530

    Update header.c

--- header.c
+++ header.c
@@ -3,6 +3,7 @@
 #include<windows.h>
 #include "header.h"
 #include "Emp_struct.c"
+#include "validation.c"
 #define SIZE 100
 
 
@@ -12,17 +13,81 @@ int employeeCount = 0;
 
 void addEmployee()
 {
+    int id ;
+    char name[25];
+    float salary;
+    int date, month, year;
     if (employeeCount < SIZE)
     {
-        printf("\nEnter EmpId = ");
-        scanf("%d", &emp[employeeCount].EmpId);
+            int flag1 = 0, flag2 = 0, flag3 = 0;
+        while(1)
+        {
+            printf("\nEnter EmpId = ");
+            scanf("%d", &id);   
+            if(isIdValid(id))
+            {
+                flag1 = 1;
+                break;
+            }
+            if(flag1 == 1)
+                break;
+            else
+            {
+                system("cls");
+                printf("\nId is invalid please enter valid Employee Id....\n Id must be numeric and positive.\n");
+            }
+        }
         printf("\nEnter Employee Name = ");
-        scanf("%s", emp[employeeCount].EmpName);
-        printf("\nEnter Employee Basic Salary = ");
-        scanf("%f", &emp[employeeCount].EmpBasicSalary);
-        printf("\nEnter Hire Date = ");
-        scanf("%d/%d/%d", &emp->HireDate.date, &emp->HireDate.month, &emp->HireDate.year);
-        employeeCount++;
+        scanf("%s", name);
+        while(1)
+        {
+            printf("\nEnter Employee Basic Salary = ");
+            scanf("%f", &salary);
+
+            if(isSalaryValid(salary))
+            {
+                flag2 = 1;
+                break;
+            }
+            if(flag2 == 1)
+                break;
+            else{
+                system("cls");
+                printf("\nSalary can not be zero or nagative.\nPlease enter Positive Salary....\n");
+            }
+
+        }
+        while(1)
+        {
+            printf("\nEnter Hire Date = ");
+            scanf("%d/%d/%d", &date, &month, &year);
+            if(isDateValid(date,month,year))
+            {
+                flag3 = 1;
+                break;
+            }
+
+            if(flag3 == 1)
+            {
+                break;
+            }else
+            {
+                   system("cls");
+                printf("\nEntered Date is Invalid....\nPlease enter Valid Date.\n");
+            }
+
+        }
+        if(flag1 == 1 && flag2 == 1 && flag3 == 1)
+        {
+            emp[employeeCount].EmpId = id;
+            strcpy(emp[employeeCount].EmpName,name);
+            emp[employeeCount].EmpBasicSalary = salary;
+            emp[employeeCount].HireDate.date = date;
+            emp[employeeCount].HireDate.month = month;
+            emp[employeeCount].HireDate.year = year;
+            
+            employeeCount++;
+        }
     }
     else
     {
@@ -37,7 +102,7 @@ void showEmployee()
 {
     system("cls");
     int id, flag = 0;
-    printf("Enter EmpId = ");
+    printf("Enter Employee Id to search = ");
     scanf("%d", &id);
     for (int i = 0; i < employeeCount; i++)
     {
@@ -72,7 +137,7 @@ float calcNetSalary(int basicSalary)
 void deleteEmployee()
 {
     int id;
-    printf("Enter Employee Id = ");
+    printf("Enter Employee Id to Delete the Employee Details = ");
     scanf("%d", &id);
     int found = 0;
     for (int i = 0; i < employeeCount; i++)
@@ -90,4 +155,41 @@ void deleteEmployee()
     }
     if (found == 0)
         printf("\nEmployee Not Found....\n");
+}
+void updateEmployee()
+{
+    int id;
+    printf("Enter Employee id to Update = ");
+    scanf("%d", &id);
+    int i;
+    char name[50];
+    float Salary;
+
+  for (i = 0; i < employeeCount; i++)
+  {
+
+    if (emp[i].EmpId == id)
+    {
+      printf("\nEnter updated Name = ");
+      scanf("%s", name);
+
+      printf("\nEnter updated Salary = ");
+      scanf("%f", &Salary);
+
+      if (strlen(name) > 0)
+      {
+        strcpy(emp[i].EmpName, name);
+      }
+
+      if (Salary > 0)
+      {
+        emp[i].EmpBasicSalary = Salary;
+      }
+
+      printf("\nDetails Updated Successfully.\n");
+      return;
+    }
+  }
+
+  printf("Employee not found.\n");
 }
